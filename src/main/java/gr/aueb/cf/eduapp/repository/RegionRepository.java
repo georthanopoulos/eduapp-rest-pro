49f8234c2ffcbecc4c9202b87da3d39de95548d7
gthanopoulos <geor.thanopoulos@gmail.com>
2026-09-05 21:48:04 +0300

    "feat: RegionRepo changes."

--- src/main/java/gr/aueb/cf/eduapp/repository/RegionRepository.java
+++ src/main/java/gr/aueb/cf/eduapp/repository/RegionRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RegionRepository extends JpaRepository<Region, Integer> {
+public interface RegionRepository extends JpaRepository<Region, Long> {
 
     List<Region> findAllByOrderByNameAsc();
 }
