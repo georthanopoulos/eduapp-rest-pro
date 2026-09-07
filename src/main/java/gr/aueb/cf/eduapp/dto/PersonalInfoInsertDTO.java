9467dc2a97de7ad1b9a3b85e45d667fe8e525a3a
gthanopoulos <geor.thanopoulos@gmail.com>
2026-09-07 19:43:18 +0300

    "feat: DTOs created"

---
+++ src/main/java/gr/aueb/cf/eduapp/dto/PersonalInfoInsertDTO.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.eduapp.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+
+@Builder                                        // Instead of bigger constructors to have a better pattern through lombok. when we have many fields
+public record PersonalInfoInsertDTO(
+
+        @NotNull
+        @Pattern(regexp = "\\d{11}")
+        String amka,
+
+        @NotBlank                                         // neither null nor empty
+        String identityNumber,
+
+        @NotBlank
+        String placeOfBirth,
+
+        @NotBlank
+        String municipalityOfRegistration
+) {}
---
+++ src/main/java/gr/aueb/cf/eduapp/dto/UserUpdateDTO.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.eduapp.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+@Builder
+public record UserUpdateDTO(
+
+        @NotBlank
+        @Size(min = 3, max = 30)
+        String username,
+
+        @NotBlank
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$")
+        String password,
+
+        @NotNull
+        Long roleId
+) {
+}
