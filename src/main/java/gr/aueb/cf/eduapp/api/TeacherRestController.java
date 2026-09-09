c62b1191db42b01fa86c694dfaaf0d9401d84612
gthanopoulos <geor.thanopoulos@gmail.com>
2026-09-09 20:02:10 +0300

    "feat: Swagger "

--- src/main/java/gr/aueb/cf/eduapp/api/TeacherRestController.java
+++ src/main/java/gr/aueb/cf/eduapp/api/TeacherRestController.java
@@ -1,9 +1,28 @@
 package gr.aueb.cf.eduapp.api;
 
+import gr.aueb.cf.eduapp.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.eduapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.eduapp.core.exceptions.ValidationException;
+import gr.aueb.cf.eduapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.eduapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.eduapp.service.ITeacherService;
+import gr.aueb.cf.eduapp.validator.TeacherInsertValidator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/teachers")
@@ -11,6 +30,48 @@ import org.springframework.web.bind.annotation.RestController;
 public class TeacherRestController {
 
     private final ITeacherService teacherService;
+    private final TeacherInsertValidator  teacherInsertValidator;
+
+
+    @Operation(
+            summary = "Save a teacher",
+            description = "Registers a new teacher in the registry."
+    )
+
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201", description = "Teacher created",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = TeacherReadOnlyDTO.class))
+            ),
+            @ApiResponse()
+
+    })
+
+
+    @PostMapping
+    public ResponseEntity<TeacherReadOnlyDTO> insertTeacher(
+            @Valid @RequestBody TeacherInsertDTO teacherInsertDTO,
+            BindingResult bindingResult                                                           // written right after TeacherInsertDTO!!!
+    ) throws EntityAlreadyExistsException, EntityInvalidArgumentException, ValidationException {
+
+        teacherInsertValidator.validate(teacherInsertDTO, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException("Teacher", "Invalid teacher data", bindingResult);
+        }
+
+        TeacherReadOnlyDTO teacherReadOnlyDTO = teacherService.saveTeacher(teacherInsertDTO);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{uuid}")
+                .buildAndExpand(teacherReadOnlyDTO.uuid())
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .body(teacherReadOnlyDTO);
+    }
 
 
 }
