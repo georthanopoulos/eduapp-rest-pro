6b06f3f313e3a12d9f5132603831ac394440942d
gthanopoulos <geor.thanopoulos@gmail.com>
2026-09-10 19:59:41 +0300

    "feat: authentication authorization and security deployment "

---
+++ src/main/java/gr/aueb/cf/eduapp/security/JwtAuthenticationFilter.java
@@ -0,0 +1,29 @@
+package gr.aueb.cf.eduapp.security;
+
+import gr.aueb.cf.eduapp.authentication.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+    }
+}
